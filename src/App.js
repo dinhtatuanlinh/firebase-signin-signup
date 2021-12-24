88c2aae0f2f18daf85cc0d3d052d0a4ad93c1f37
linh-dinh <linh.dinh@overdose.digital>
2021-12-24 18:40:06 +0700

    up

--- src/App.js
+++ src/App.js
@@ -78,6 +78,7 @@ function App() {
     function loginWithGoogle(){
         signInWithPopup(auth, googleProvider)
         .then((result) => {
+            console.log(12343);
             // This gives you a Google Access Token. You can use it to access the Google API.
             const credential = GoogleAuthProvider.credentialFromResult(result);
             const token = credential.accessToken;
